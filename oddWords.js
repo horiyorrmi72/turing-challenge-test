1ffb6c887555e1311db4beabab26211ece7026ce
Yusuf Ridwan <horiyorrmi72@gmail.com>
2023-02-24 18:10:01 +0100

    finding odd words in two different strings

---
+++ oddWords.js
@@ -0,0 +1,21 @@
+// function oddWords(str1, str2) {
+//     return str.split(' ').filter((word, index) => index % 2 !== 0).join(' ');
+// }
+
+// console.log(oddWords(['This is a test'],['this is a normal']));
+function findOddWords(str1, str2){
+    let arr1 = str1.split(' ');
+    let arr2 = str2.split(' ');
+    let result = [];
+    for(let i = 0; i < arr1.length; i++){
+        for(let j = 0; j < arr2.length; j++){
+            if(!arr2.includes(arr1[i]) && !arr1.includes(arr2[j])){
+                result.push(arr1[i]);
+                result.push(arr2[j]);
+            }
+        }
+    }
+    return result;
+}
+
+console.log(findOddWords('This is a test','This is a normal'));
